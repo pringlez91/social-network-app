2c0805ce427ce9c737ae8488c86928af9d44ce95
pringlez91 <ahmedellisy@outlook.com>
2020-01-21 07:01:17 -0330

    profile image incomplete

--- app/src/main/java/com/example/social_network_app/SetupActivity.java
+++ app/src/main/java/com/example/social_network_app/SetupActivity.java
@@ -1,9 +1,11 @@
 package com.example.social_network_app;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -13,9 +15,15 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.util.HashMap;
 
@@ -29,8 +37,10 @@ public class SetupActivity extends AppCompatActivity {
 
   private FirebaseAuth mAuth;
   private DatabaseReference UsersRef;
+  private StorageReference imageRef;
 
   String currentUser;
+  static final int Pic = 1;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +50,7 @@ public class SetupActivity extends AppCompatActivity {
     mAuth = FirebaseAuth.getInstance();
     currentUser = mAuth.getCurrentUser().getUid();
     UsersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUser);
+    imageRef = FirebaseStorage.getInstance().getReference().child("profile img");
 
     UserName = (EditText) findViewById(R.id.setup_username);
     FullName = (EditText) findViewById(R.id.setup_fn);
@@ -54,6 +65,79 @@ public class SetupActivity extends AppCompatActivity {
             SaveAccount();
           }
         });
+
+    profileImage.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+
+            Intent galIntent = new Intent();
+            galIntent.setAction(Intent.ACTION_GET_CONTENT);
+            galIntent.setType("image/*");
+            startActivityForResult(galIntent, Pic);
+          }
+        });
+  }
+
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    if (requestCode == Pic && resultCode == RESULT_OK && data != null) {
+      Uri ImageUri = data.getData();
+      CropImage.activity()
+          .setGuidelines(CropImageView.Guidelines.ON)
+          .setAspectRatio(1, 1)
+          .start(this);
+    }
+
+    if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+      CropImage.ActivityResult result = CropImage.getActivityResult(data);
+      if (requestCode == RESULT_OK) {
+        Uri resultUri = result.getUri();
+
+        StorageReference path = imageRef.child(currentUser + ".jpg");
+
+        path.putFile(resultUri)
+            .addOnCompleteListener(
+                new OnCompleteListener<UploadTask.TaskSnapshot>() {
+                  @Override
+                  public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+
+                    if (task.isSuccessful()) {
+
+                      final String imgURL =
+                          task.getResult().getStorage().getDownloadUrl().toString();
+                      UsersRef.child("profileimg")
+                          .setValue(imgURL)
+                          .addOnCompleteListener(
+                              new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+
+                                  if (task.isSuccessful()) {
+                                    Intent setup =
+                                        new Intent(SetupActivity.this, SetupActivity.class);
+                                    startActivity(setup);
+                                    Toast.makeText(
+                                            SetupActivity.this,
+                                            "Profile Image uploaded",
+                                            Toast.LENGTH_LONG)
+                                        .show();
+
+                                  } else {
+                                    String msg = task.getException().getMessage();
+                                    Toast.makeText(
+                                            SetupActivity.this, "Error: " + msg, Toast.LENGTH_LONG)
+                                        .show();
+                                  }
+                                }
+                              });
+                    }
+                  }
+                });
+      }
+    }
   }
 
   private void SaveAccount() {
